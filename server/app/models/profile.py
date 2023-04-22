d9bb14fa8a22ab66c9c7e4d4bac280cfb6ce70da
Dušan Vystrčil <82444947+DusanVystrcil@users.noreply.github.com>
2023-04-22 15:02:16 +0200

    New tables for profile

---
+++ server/app/models/profile.py
@@ -0,0 +1,20 @@
+from app import db
+
+class Profile(db.Model):
+    dimension = db.Column(db.String(255),primary_key=True, nullable=False)
+    summary = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, dimension, summary):
+        self.dimension = dimension
+        self.summary = summary
+
+    @classmethod
+    def update_dimension(cls, dimension, new_summary):
+        new_values = {
+            'dimension': dimension,
+            'summary': new_summary
+        }
+        new_dimension = cls(**new_values)
+        db.session.add(new_dimension)
+        db.session.commit()
+        return new_dimension
