368f471b6cfb7776c779f05d65d80a5fb5250e0f
0xdesigner <125830405+0xdesigner@users.noreply.github.com>
2023-04-22 13:58:20 +0200

    Update Navbar.js

--- client/src/components/Main/Navbar.js
+++ client/src/components/Main/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
   return(
     <nav className="w-full flex-none px-4 bg-bgblack">
     <div className="flex w-6/12" style={{ position: 'absolute', top: '10px'}}>
-      <img src="../assets/images/avatar.png" alt="Ai hackaton" className="w-12 h-12 rounded-full mr-4 mt-1"/> <h2 className="text-white text-lg display-2 flex content-left items-center ">aihackaton.vercel.app</h2>
+      <img src="../assets/images/avatar.png" alt="Ai hackaton" className="w-12 h-12 rounded-full mr-4 mt-1"/> <h2 className="text-white text-lg display-2 flex content-left items-center ">Coach Ai</h2>
       </div>
     </nav>
   )
